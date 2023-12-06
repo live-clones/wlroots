34ec9319d0bd4705366fd880950643f35c8cfdcc
Alexander Orzechowski <alex@ozal.ski>
2023-12-05 20:36:52 -0500

    wlr_output: Make wlr_output_effective_resolution take state

--- examples/output-layers.c
+++ examples/output-layers.c
@@ -92,7 +92,7 @@ static void output_handle_frame(struct wl_listener *listener, void *data) {
 	}
 
 	int width, height;
-	wlr_output_effective_resolution(output->wlr_output, &width, &height);
+	wlr_output_effective_resolution(output->wlr_output, NULL, &width, &height);
 
 	struct wlr_render_pass *pass = wlr_output_begin_render_pass(output->wlr_output, &output_state, NULL);
 	wlr_render_pass_add_rect(pass, &(struct wlr_render_rect_options){
