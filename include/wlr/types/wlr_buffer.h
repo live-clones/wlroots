afc6ac23f5308442a978fc31590a5e9d05b0c420
Alexander Orzechowski <alex@ozal.ski>
2025-01-25 18:22:41 -0500

    buffer: actually make wlr_buffer_is_opaque public
    Symbols won't be exported if they aren't present in the include/wlr
    folder. Simply prefixing wlr_ is not enough.

--- include/wlr/types/wlr_buffer.h
+++ include/wlr/types/wlr_buffer.h
@@ -172,4 +172,13 @@ struct wlr_client_buffer {
  */
 struct wlr_client_buffer *wlr_client_buffer_get(struct wlr_buffer *buffer);
 
+/**
+ * Check whether a buffer is fully opaque.
+ *
+ * When true is returned, the buffer is guaranteed to be fully opaque, but the
+ * reverse is not true: false may be returned in cases where the buffer is fully
+ * opaque.
+ */
+bool wlr_buffer_is_opaque(struct wlr_buffer *buffer);
+
 #endif
