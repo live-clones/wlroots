913243e1729c08d7e42d045977226af2c4476e68
Félix Poisot <felix@lhfblc.fr>
2025-01-31 18:43:23 +0000

    drm/syncobj: add timeline point merger utility

---
+++ include/render/drm_syncobj_merger.h
@@ -0,0 +1,40 @@
+#ifndef WLR_RENDER_DRM_SYNCOBJ_MERGER_H
+#define WLR_RENDER_DRM_SYNCOBJ_MERGER_H
+
+#include <wayland-server-core.h>
+
+/**
+ * Accumulate timeline points, to have a destination timeline point be
+ * signalled when all inputs are
+ */
+struct wlr_drm_syncobj_merger {
+	int n_ref;
+	struct wlr_drm_syncobj_timeline *dst_timeline;
+	uint64_t dst_point;
+	int sync_fd;
+};
+
+/**
+ * Create a new merger.
+ *
+ * The given timeline point will be signalled when all input points are
+ * signalled and the merger is destroyed.
+ */
+struct wlr_drm_syncobj_merger *wlr_drm_syncobj_merger_create(
+	struct wlr_drm_syncobj_timeline *dst_timeline, uint64_t dst_point);
+/**
+ * Target timeline point is materialized when all inputs are, and the merger is
+ * destroyed.
+ */
+void wlr_drm_syncobj_merger_commit_and_destroy(struct wlr_drm_syncobj_merger *merger);
+/**
+ * Add a new timeline point to wait for.
+ *
+ * If the point is not materialized, the supplied event loop is used to schedule
+ * a wait.
+ */
+bool wlr_drm_syncobj_merger_add(struct wlr_drm_syncobj_merger *merger,
+	struct wlr_drm_syncobj_timeline *dst_timeline, uint64_t dst_point,
+	struct wl_event_loop *loop);
+
+#endif
