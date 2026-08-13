live-clones/wlroots

commit be555a9a99a4b4deb9ad72b4ed25f39ba5352d0d

backend/drm: make drm_lease implementation follow docs
This ensures compositors get a output_destroy signal when the
lease is granted and a new_output signal when the lease is revoked.

backend/drm/drm.c
@@ -1762,6 +1762,10 @@ void scan_drm_connectors(struct wlr_drm_backend *drm,
 			}
 		}
 
+		if (wlr_conn && wlr_conn->lease) {
+			continue;
+		}
+
 		// If the hotplug event contains a connector ID, ignore any other
 		// connector.
 		if (event != NULL && event->connector_id != 0 &&
@@ -2150,6 +2154,7 @@ struct wlr_drm_lease *wlr_drm_create_lease(struct wlr_output **outputs,
 				get_drm_connector_from_output(outputs[i]);
 		conn->lease = lease;
 		conn->crtc->lease = lease;
+		disconnect_drm_connector(conn);
 	}
 
 	return lease;
@@ -2186,4 +2191,5 @@ void drm_lease_destroy(struct wlr_drm_lease *lease) {
 	}
 
 	free(lease);
+	scan_drm_connectors(drm, NULL);
 }

commit a58b1a7a759449dcd52e89a4c0af136dd03f8622

backend/drm: Fix lease termination when multiple outputs are leased.
Make sure that a scan_drm_connectors() marks a wlr_conn which
has active leases as "seen", before skipping further processing
in the first pass scan loop.

Otherwise, while leasing multipe connectors to Wayland clients once
will work, terminating the leases won't work properly, and only
dealing with and terminating one lease for exactly one connector
works, leaving the remaining leased ones broken.

Observed failure scenario:

1. Client leases n connectors in separate lease fd's.
2. Client uses the leases successfully, yay!
3. Client terminates one lease (via close(fd); on the leased fd)
4. Linux kernel revokes and cleanes up the lease properly,
   sends a udev lease changed event to wlroots.
5. wlroots responds by executing scan_drm_leases() to find the
   wlr_conn corresponding to the revoked lease in the kernel,
   finds it and destroys it / cleans it up via a call to
   drm_lease_destroy(), and sends out the relevant lease events to
   the client, makes an output available for leasing again etc.
   So far, so good... We now have n-1 leases left in wlroots and
   the Linux kernel.

Now trouble begins for the remaining active leases:

6. drm_lease_destroy() calls scan_drm_connectors() at the end of
   its execution.
7. scan_drm_connectors() in its first loop, whenever it encounters
   one of the remaining n-1 wlr_conn's which are leased out, skips
   processing of the wlr_conn without setting its seen[index]=true;
8. The following reverse scan loop destroys all wlr_conn's which
   don't have seen[index]==true, so all remaining n-1 wlr_conn's
   with active leases get destroyed via destroy_drm_connector(conn),
   and wlroots "forgets" about their existence and the state of
   their leases! More specifically: When other leases are revoked
   by the kernel (due to close(fd)), all the sent out udev lease
   change events will trigger runs of scan_drm_leases(), but
   those scans turn into a no-op, because they can't find the connectors
   corresponding to the leases anymore: No lease related events are
   sent to Wayland clients anymore, clients get confused, Xwayland
   gets confused, etc.

Therefore the remaining n-1 leases can not be properly given back
to wlroots after their leasing Wayland clients have terminated their
use. At least not until some other factors trigger another fresh
scan_drm_connectors() run, e.g., compositor restart, VT switch,
other lease terminating events.

This was found during testing of leasing of multiple outputs via
Psychtoolbox, implemented as a X11 Vulkan client, using the X11 Vulkan
leasing extensions like vkAcquireXlibDisplay(), vkReleaseDisplay()
It used X11 RandR output leasing, running under Xwayland on top of
labwc on top of wlroots, to lease multiple outputs, uses them, and
then terminates its leases again in a for loop by closing the lease
fd's one by one (Mesa's Vulkan/WSI/display does that). As a result
of the above, the kernel terminated all leases successfully, bringing
labwc's desktop back on the screens, but all wl_outputs / Xwayland RandR
outputs where then in a damaged state, unleasable etc., except
for the 1st output that was originally leased out.

Signed-off-by: Mario Kleiner <mario.kleiner.de@gmail.com>
Fixes: be555a9a99a4

backend/drm/drm.c
@@ -1810,6 +1810,7 @@ void scan_drm_connectors(struct wlr_drm_backend *drm,
 		}
 
 		if (wlr_conn && wlr_conn->lease) {
+			seen[index] = true;
 			continue;
 		}
 